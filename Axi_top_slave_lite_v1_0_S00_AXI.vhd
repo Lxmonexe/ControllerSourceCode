ec81cf9650842710cdd13a0f372b828cbcff71f5
Lxmonexe <gabin_blanchet@etu.u-bourgogne.fr>
2025-06-23 09:20:19 +0200

    Update slv_reg5 initialization value in Axi_top_slave_lite for correct register setup

--- Axi_top_slave_lite_v1_0_S00_AXI.vhd
+++ Axi_top_slave_lite_v1_0_S00_AXI.vhd
@@ -125,7 +125,7 @@ architecture arch_imp of Axi_top_slave_lite_v1_0_S00_AXI is
 	signal slv_reg2	:std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
 	signal slv_reg3	:std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
 	signal slv_reg4	:std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
-	signal slv_reg5	:std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0) := x"00020003";
+	signal slv_reg5	:std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0) := x"00020004";
 	signal byte_index	: integer;
 
 	 signal mem_logic  : std_logic_vector(ADDR_LSB + OPT_MEM_ADDR_BITS downto ADDR_LSB);
