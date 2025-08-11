637918e8082c3c822b47d7c4c6268f70d0002a2c
Lxmonexe <gabin_blanchet@etu.u-bourgogne.fr>
2025-08-11 14:16:01 +0200

    Refactor test_axi.py for improved readability and consistency in comments and code structure

--- testbench/test_axi.py
+++ testbench/test_axi.py
@@ -5,8 +5,8 @@ from cocotbext.axi import AxiLiteMaster, AxiLiteBus
 
 async def clk_gen(dut,clk, period1_ns=10, period2_ns=8):  
     """ Clock generator """
-    cocotb.start_soon(Clock(dut.clk_controller_i, period1_ns, units="ns").start())
-    cocotb.start_soon(Clock(clk, period2_ns, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk_controller_i, period1_ns, units="ns").start()) #controller clock
+    cocotb.start_soon(Clock(clk, period2_ns, units="ns").start()) #AXI clock
 
 @cocotb.test()
 async def run_test(dut):
@@ -14,21 +14,21 @@ async def run_test(dut):
     
     clk = dut.s00_axi_aclk
     rst = dut.s00_axi_aresetn
+    
     dut.dbg_btn_i.value = 0
-    controller_ready = None
+    
     await clk_gen(dut, clk)   
-    axi_master = AxiLiteMaster(AxiLiteBus.from_prefix(dut, "s00_axi"), clk, rst,reset_active_level=False)
+    
+    axi_master = AxiLiteMaster(AxiLiteBus.from_prefix(dut, "s00_axi"), clk, rst,reset_active_level=False) # create an AxiLiteMaster instance
    
-    
-    
-    
+    # Reset Axi interface 
     rst.value = 0
     for _ in range(5):
         await RisingEdge(clk)
     rst.value = 1
     await RisingEdge(clk)
 
-    await Timer(12000, units="ns")  # Wait for the controller to be ready
+    await Timer(12000, units="ns")  # Wait to let the flash power up
     
     dut.dbg_btn_i.value = 1
     
@@ -38,64 +38,59 @@ async def run_test(dut):
     await axi_master.write(0x00004, (1).to_bytes(4, 'little'))
     
     
-
+    # Wait for the reset to complete
     while True:
         resp = await axi_master.read(0x00000, 4)
         data = int.from_bytes(resp.data, byteorder="little")
-        if data & (1 << 0):  # Vérifie si le bit 0 est à 1
+        if data & (1 << 0):   
             print("reset done")
             await axi_master.write(0x00004, (0).to_bytes(4, 'little'))
             break
-        await Timer(1000, units="ns")  # Petite pause pour éviter de surcharger la simulation
+        await Timer(1000, units="ns")  
 
     
-
     # READ ID command
-
     await axi_master.write(0x0000c, (32).to_bytes(4, 'little'))
-    await Timer(300, units="ns")
     await axi_master.write(0x00008, (2).to_bytes(4, 'little'))
-    await Timer(300, units="ns")
     await axi_master.write(0x00004, (1).to_bytes(4, 'little'))
 
     while True:
         resp = await axi_master.read(0x00000, 4)
         data = int.from_bytes(resp.data, byteorder="little")
-        if (data & (1 << 0)) and (data & (1 << 1)):  # Vérifie bit 0 et bit 1
+        if (data & (1 << 0)) and (data & (1 << 1)):  
             
             await axi_master.write(0x00004, (0).to_bytes(4, 'little')) 
             print("command done")
             break
         await Timer(1000, units="ns")
 
+    # Write data into the BRAM
     for i in range(2300):
         await axi_master.write(0x30000 + i * 4, (151520258).to_bytes(4, 'little'))
 
+    # Write data command
     await axi_master.write(0x0000c, (67108864).to_bytes(4, 'little'))
-    await Timer(300, units="ns")
     await axi_master.write(0x00008, (32800).to_bytes(4, 'little'))
-    await Timer(300, units="ns")
     await axi_master.write(0x00004, (1).to_bytes(4, 'little'))
 
     while True:
         resp = await axi_master.read(0x00000, 4)
         data = int.from_bytes(resp.data, byteorder="little")
-        if (data & (1 << 0)) and (data & (1 << 1)):  # Vérifie bit 0 et bit 1
+        if (data & (1 << 0)) and (data & (1 << 1)):  
             
             await axi_master.write(0x00004, (0).to_bytes(4, 'little')) 
             print("command done")
             break
         await Timer(1000, units="ns")
 
-    await Timer(300, units="ns")
+    # Read data command
     await axi_master.write(0x00008, (33808).to_bytes(4, 'little'))
-    await Timer(300, units="ns")
     await axi_master.write(0x00004, (1).to_bytes(4, 'little'))
 
     while True:
         resp = await axi_master.read(0x00000, 4)
         data = int.from_bytes(resp.data, byteorder="little")
-        if (data & (1 << 0)) and (data & (1 << 1)):  # Vérifie bit 0 et bit 1
+        if (data & (1 << 0)) and (data & (1 << 1)):  
             
             await axi_master.write(0x00004, (0).to_bytes(4, 'little')) 
             print("command done")
